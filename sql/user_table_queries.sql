6c626307593d91acba465cd6f1b9fd4203059ad0
koala-man-64 <rdprokes@gmail.com>
2025-03-20 23:17:46 -0500

    restructuring

---
+++ sql/user_table_queries.sql
@@ -0,0 +1,30 @@
+CREATE TABLE dbo.Users (
+    UserID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    [Password] NVARCHAR(200) NOT NULL,
+    CreatedDate DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+    LastUpdatedDate DATETIME2 NOT NULL DEFAULT GETUTCDATE()
+);
+
+
+INSERT INTO dbo.Users (Username, [Password])
+VALUES ('admin', '390a4ab5-a138-48d5-b12f-8b172157c56e');
+
+UPDATE dbo.Users
+SET [Password] = 'NewP@ssword456', 
+    LastUpdatedDate = GETUTCDATE()
+WHERE Username = 'john_doe';
+
+
+
+INSERT INTO dbo.Users (Username, [Password])
+VALUES ('john_doe', HASHBYTES('SHA2_256', 'P@ssword123'));
+
+UPDATE dbo.Users
+SET [Password] = HASHBYTES('SHA2_256', 'NewP@ssword456'),
+    LastUpdatedDate = GETUTCDATE()
+WHERE Username = 'john_doe';
+
+
+DELETE FROM dbo.Users
+WHERE Username = 'john_doe';
