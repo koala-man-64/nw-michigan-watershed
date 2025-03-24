d29eedb7d23943f7878595c5f22385e61cdc868d
koala-man-64 <rdprokes@gmail.com>
2025-03-23 22:56:35 -0500

    fdsa

--- api/hello_world/hello_world.py
+++ api/hello_world/hello_world.py
@@ -1,6 +1,6 @@
 import logging
 import azure.functions as func
 
-def main(req: func.HttpRequest, res: func.Out[str]) -> None:
-    logging.info('Python HTTP trigger function processed a request.')
-    res.set("Hello, world!")
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Hello World function processed a request.")
+    return func.HttpResponse("Hello, World!", status_code=200)
