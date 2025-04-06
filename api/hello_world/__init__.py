65d46a7529bbf891ff02eb98002664e9d65db088
koala-man-64 <rdprokes@gmail.com>
2025-04-06 10:22:40 -0500

    fixing common ref

--- api/hello_world/__init__.py
+++ api/hello_world/__init__.py
@@ -2,7 +2,7 @@ import logging
 import azure.functions as func
 import sys
 import os
-import common
+# import common
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Hello World function processed a request.")
