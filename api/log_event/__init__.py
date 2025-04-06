5d16013da599f845b68d0a143c0b9ecc29a35551
koala-man-64 <rdprokes@gmail.com>
2025-04-06 11:08:00 -0500

    fixing common ref

--- api/log_event/__init__.py
+++ api/log_event/__init__.py
@@ -7,20 +7,20 @@ import common
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Received a log event request.")
 
-    # try:
-    #     req_body = req.get_json()
-    # except ValueError:
-    #     return func.HttpResponse("Invalid JSON", status_code=400)
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        return func.HttpResponse("Invalid JSON", status_code=400)
 
-    # # Retrieve each value, defaulting to an empty string if None
-    # eventType      = req_body.get("eventType") or ""
-    # targetTag      = req_body.get("targetTag") or ""
-    # targetId       = req_body.get("targetId") or ""
-    # targetClasses  = req_body.get("targetClasses") or ""
-    # targetText     = req_body.get("targetText") or ""
-    # timestamp      = req_body.get("timestamp") or ""
-    # clientIp       = req_body.get("clientIp") or ""
-    # clientUrl      = req_body.get("clientUrl") or ""
+    # Retrieve each value, defaulting to an empty string if None
+    eventType      = req_body.get("eventType") or ""
+    targetTag      = req_body.get("targetTag") or ""
+    targetId       = req_body.get("targetId") or ""
+    targetClasses  = req_body.get("targetClasses") or ""
+    targetText     = req_body.get("targetText") or ""
+    timestamp      = req_body.get("timestamp") or ""
+    clientIp       = req_body.get("clientIp") or ""
+    clientUrl      = req_body.get("clientUrl") or ""
 
     # try:
     #     connection_string = cf.get_connection_string()
