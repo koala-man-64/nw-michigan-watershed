ca0a81fad775613d79476e448735f930acd52fad
koala-man-64 <rdprokes@gmail.com>
2026-03-20 22:17:05 -0500

    c

--- client/src/map/mapViewport.test.js
+++ client/src/map/mapViewport.test.js
@@ -12,7 +12,7 @@ describe("mapViewport", () => {
     expect(MAP_DEFAULT_CENTER).toEqual([44.75, -85.85]);
     expect(MAP_DEFAULT_ZOOM).toBe(8);
     expect(MAP_MIN_ZOOM).toBe(7);
-    expect(MAP_MAX_ZOOM).toBe(11);
+    expect(MAP_MAX_ZOOM).toBe(16);
     expect(MAP_MAX_BOUNDS_VISCOSITY).toBe(0.8);
     expect(NW_MICHIGAN_MAX_BOUNDS).toEqual([
       [44.14, -86.4],
